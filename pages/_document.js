73e7d6de144a40c915f2d7fe58d7fb8d57435ed2
J Weatherby <jamie.weatherby@gmail.com>
2022-02-27 15:49:20 -0500

    Add matomo tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 
 const getActiveStyle = (path, pattern) => {
@@ -40,6 +41,20 @@ export default class App extends Document {
               <NextScript />
             </div>
           </main>
+          <Script type="text/javascript" dangerouslySetInnerHTML={{
+            __html: `
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://showboard.matomo.cloud/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/showboard.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    `}} />
         </body>
       </Html>
     );
