9974b36591ae8f965c23c3bd8dae60b17732d34c
J Weatherby <jamie.weatherby@gmail.com>
2022-02-28 22:48:35 -0500

    Add url and html to body for parsing

--- pages/api/get-readable-html.js
+++ pages/api/get-readable-html.js
@@ -3,11 +3,8 @@ import { JSDOM } from 'jsdom'
 
 export default function handler(request, response) {
 
-  const doc = new JSDOM(
-    request.body,
-    {
-      title: 'https://www.bbc.co.uk/blogs/internet/entries/41db1cb0-8948-40a9-b2be-22c9494078f6'
-    }).window.document
+  const { url, html } = request.body
+  const doc = new JSDOM(html, { url }).window.document
 
   if (!isProbablyReaderable(doc)) {
     return response.status(200).json({
