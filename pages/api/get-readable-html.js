fc68fe64d307c5d63d0e3627799d84c3288a7ff2
J Weatherby <jamie.weatherby@gmail.com>
2022-02-28 22:24:49 -0500

    Add JSDOM reader to readability

--- pages/api/get-readable-html.js
+++ pages/api/get-readable-html.js
@@ -1,24 +1,25 @@
 import { Readability, isProbablyReaderable } from '@mozilla/readability'
 import { JSDOM } from 'jsdom'
 
-
 export default function handler(request, response) {
 
-  const doc = new JSDOM(request.body)
+  const doc = new JSDOM(
+    request.body,
+    {
+      title: 'https://www.bbc.co.uk/blogs/internet/entries/41db1cb0-8948-40a9-b2be-22c9494078f6'
+    }).window.document
+
   if (!isProbablyReaderable(doc)) {
     return response.status(200).json({
-      body: { isReadable: false }
+      isReadable: false
     })
   }
-  const { title, content } = new Readability(doc).parse()
+
+  const { title, content } = (new Readability(doc)).parse()
 
   response.status(200).json({
-    body: {
-      isReadable: true,
-      content,
-      title,
-    },
-    query: request.query,
-    cookies: request.cookies,
+    isReadable: false,
+    title,
+    content
   });
 }
