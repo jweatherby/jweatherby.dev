f897a1aef05a207a20890a449847e5ccdc247efe
jweatherby <jamie.weatherby@gmail.com>
2026-05-25 12:12:02 -0400

    Add sandbox.paperwall token

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,8 @@
   <script type="application/javascript">
     window.onload = () =>
       buildWall({
-        siteToken: "Qhf7zPQWSi4",
+        // siteToken: "Qhf7zPQWSi4", // paperwall.io
+        siteToken: 'cFVyMdqPz3w', // sandbox.paperwall.io
         articleFinder: {
           selector: "blog-post",
           postUrls: [/\/posts\/.*/],
